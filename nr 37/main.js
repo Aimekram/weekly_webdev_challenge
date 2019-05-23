42d919163e558f543f2f4c25acc39bc1f15e4f4b
Aimekram <janina.byszewska@gmail.com>
2019-05-23 22:52:50 +0200

    all svgs responsive

--- nr 37/main.js
+++ nr 37/main.js
@@ -1,17 +1,31 @@
-let leftHeight = document.getElementById("left").clientHeight;
+function changeHeightSmallLeft() {
+    const sectionHeight = document.getElementById("services").clientHeight;
+    const sectionWidth = document.getElementById("services").clientWidth;
+    document.getElementById("leftxs").style.height = sectionHeight * 0.5;
+    document.getElementById("leftxs").style.top = (sectionHeight * 0.5)/2 - sectionWidth * 0.05; 
+};
 
-//leftHeight = sectionHeight + 80px;
 
-function changeHeight() {
-    let sectionHeight = document.getElementById("clients").clientHeight;
-    let sectionWidth = document.getElementById("clients").clientWidth;
-    
+function changeHeightBigRight() {
+    const sectionHeight = document.getElementById("process").clientHeight;
+    const sectionWidth = document.getElementById("process").clientWidth;
+    document.getElementById("right").style.height = sectionHeight + 300;
+    document.getElementById("right").style.top = - (sectionWidth * 0.05 + 150)
+}
+
+function changeHeightBigLeft() {
+    const sectionHeight = document.getElementById("clients").clientHeight;
+    const sectionWidth = document.getElementById("clients").clientWidth;
     document.getElementById("left").style.height = sectionHeight + 140;
-    console.log("left: " + leftHeight + "; container: " + sectionHeight);
-    
     document.getElementById("left").style.top = - (sectionWidth * 0.05 + 70)
 };
 
-changeHeight();
+function changeHeight() {
+    changeHeightBigLeft();
+    changeHeightBigRight();
+    changeHeightSmallLeft();
+};
 
+
+changeHeight();
 window.onresize = changeHeight;
