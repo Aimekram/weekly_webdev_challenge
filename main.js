158194af9478876ebb8386bda6b953cd063d6c83
Aimekram <janina.byszewska@gmail.com>
2019-06-24 16:18:44 +0200

    page fully done

--- main.js
+++ main.js
@@ -9,8 +9,14 @@ function changeHeightSmallLeft() {
 function changeHeightBigRight() {
     const sectionHeight = document.getElementById("process").clientHeight;
     const sectionWidth = document.getElementById("process").clientWidth;
-    document.getElementById("right").style.height = sectionHeight + 300;
-    document.getElementById("right").style.top = - (sectionWidth * 0.05 + 150)
+    
+    if (sectionWidth < 768) {
+        document.getElementById("right").style.height = sectionHeight;
+        document.getElementById("right").style.top = sectionHeight * .2;
+    } else {
+        document.getElementById("right").style.height = sectionHeight + 300;
+        document.getElementById("right").style.top = - (sectionWidth * 0.05 + 150);
+    }
 }
 
 function changeHeightBigLeft() {
@@ -29,3 +35,44 @@ function changeHeight() {
 
 changeHeight();
 window.onresize = changeHeight;
+
+
+//--------------------------------------slider
+
+const leftArrow = document.getElementById("leftarrow");
+const rightArrow = document.getElementById("rightarrow");
+
+const slidesArr = document.querySelectorAll(".slider_item")
+let currentSlide = 0;
+
+function changeSlide(direction) {
+    let first;
+    let second;
+    
+    if (direction === 1) {
+        prev = "prev";
+        next = "next";
+    } else {
+        prev = "next";
+        next = "prev";
+    }
+    
+    slidesArr[currentSlide].className = 'slider_item ' + prev;
+    currentSlide = (currentSlide + direction) % slidesArr.length;
+    slidesArr[currentSlide].className = 'slider_item show';
+    slidesArr[(currentSlide + direction) % slidesArr.length].className = 'slider_item ' + next;
+    console.log(currentSlide);
+}
+
+function prevSlide() {
+  changeSlide(slidesArr.length-1)
+}
+
+function nextSlide() {
+  changeSlide(1)
+}
+
+//const slideInterval = setInterval(nextSlide,2000);
+
+leftArrow.onclick = prevSlide;
+rightArrow.onclick = nextSlide;
